3b1b705985e8511793440e2a5ee8a6040ae5a6a8
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-22 07:55:52 +0300

    connecting to mongoose MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect Database
+connectDB();
 
 app.get('/', () => res.send('API running'));
 
