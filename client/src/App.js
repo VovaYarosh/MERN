95a8ac0a4e78edc5d818427b2d5c6d98cd09d904
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-28 09:23:52 +0300

    create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile-forms/CreateProfile'
+
 
 import './App.css';
 
@@ -39,6 +41,7 @@ const App = () => {
                         <Route exact path="/register" component={Register}/>
                         <Route exact path="/login" component={Login}/>
                         <PrivateRoute Route exact path="/dashboard" component={Dashboard}/>
+                        <PrivateRoute Route exact path="/create-profile" component={CreateProfile}/>
                     </Switch>
                 </section>
                 <Landing/>
